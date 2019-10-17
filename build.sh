bdb234d626ce33952791b4e4e7f3735a4a7628aa
BrianLian <brianlian1229@gmail.com>
2019-10-17 10:06:30 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+XCAT3_K=~/intrising/xcat3-cpss/linux/uni/4.4/kernel/mvebu_v7_lsp_noprestera_defconfig
+KDIR=$XCAT3_K ARCH=arm CROSS_COMPILE=arm-marvell-linux-gnueabi- make
+
+echo "Please check exfat.ko"
+
